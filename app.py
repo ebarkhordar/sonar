eeec8117289d35efcc1f18fd1c4836a5cdeea8ee
Ehsan Barkhordar <barkhordar.me@gmail.com>
2022-11-08 22:10:58 +0300

    add metrics

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Welcome to Divar!'
+@app.route('/', methods=["POST", "GET"])
+def main():
+    print(request.method)
+    user_data = {}
+    if request.method == 'POST':
+        user_data.update({"ip": request.remote_addr})
+        return user_data
+    return render_template('index.html', user_data=user_data)
 
 
 if __name__ == '__main__':
